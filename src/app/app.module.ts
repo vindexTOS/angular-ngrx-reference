02db049875dc5e7b2dcbaee4da2c021131d11cca
VindexTOS <giorgikutateladze1998@gmail.com>
2023-10-07 23:04:59 +0400

    init server nest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
-import { NgModule } from '@angular/core'
+import { NgModule, isDevMode } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { StoreModule } from '@ngrx/store'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { CounterbuttonComponent } from './component/counterbutton/counterbutton.component'
-import { CounterdisplayComponent } from './component/counterdisplay/counterdisplay.component'
-import { counterReducer } from './shared/store/counter.reducer'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './Material.Module'
-import { CustomcounterComponent } from './component/customcounter/customcounter.component'
 import { FormsModule } from '@angular/forms'
+
+import { HttpClientModule } from '@angular/common/http'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store'
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterbuttonComponent,
-    CounterdisplayComponent,
-    CustomcounterComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ counter: counterReducer }),
+    StoreModule.forRoot({}),
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
+    HttpClientModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/store/counter.state.ts
+++
@@ -1,9 +0,0 @@
-export interface counterModel {
-  counter: number
-  channelname: string
-}
-
-export const initialState: counterModel = {
-  counter: 0,
-  channelname: 'gio',
-}
