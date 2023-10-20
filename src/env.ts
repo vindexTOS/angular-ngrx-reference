79fc5fca6b142d2b335fd818e138218c408bde29
Giorgi <110175298+vindexTOS@users.noreply.github.com>
2023-10-20 23:56:22 +0400

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  // in production i would have used .env :)
-  production: false,
+   production: false,
   apiUrl: 'https://development.api.optio.ai/api/v2/',
   apiAuthToken:
     'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoiZy5rdXRhdGVsaUBnbWFpbC5jb20iLCJpYXQiOjE2OTc0MzAyODAsImV4cCI6MTY5ODI5NDI4MH0.f-e-7LFCZaHNpSbZtluSzCX8TpSkzUVWfQxogou7OEOJ2unyjxDLRNuPTlgFYu489s7FqDJPtJdSQ-hQlYIIow',
